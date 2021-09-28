bfc34862f319fbccb9b7c53f6961fbf6ec3a9db2
Te Wei <tw2159@nyu.edu>
2021-09-28 15:11:33 -0700

    generate a webpage

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
-const profileDataArgs = process.argv.slice(2);
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const [name, github] = profileDataArgs;
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
 
-  console.log('================');
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
