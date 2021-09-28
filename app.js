73aa1274170cd8fe8342fbe01cc2783e9a833d96
Te Wei <tw2159@nyu.edu>
2021-09-28 16:13:35 -0700

    create the gitignore

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const fs = require('fs');
-const [name, github] = profileDataArgs;
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const [name, github] = profileDataArgs;
+// const generatePage = require('./src/page-template.js');
 
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw err;
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//   if (err) throw err;
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
